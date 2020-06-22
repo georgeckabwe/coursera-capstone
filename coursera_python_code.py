099993eda2bd6afaa908d18640f6aeb0e547a0f9
georgeckabwe <georgeckabwe@gmail.com>
2020-06-22 22:57:12 +0200

    adds a python script that imports pandas as pd and numpy as np and prints hello world.

---
+++ coursera_python_code.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # COURSERA CAPSTONE
+# This notebook will be mainly used for the capstone project
+# 
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
